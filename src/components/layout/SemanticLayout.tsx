4db561aeecaf17a67c746aa9b3704e8542438c60
DataOps Group <GEOFF@dataopsgroup.com>
2025-05-26 16:41:32 -0500

    Update SemanticLayout.tsx

--- src/components/layout/SemanticLayout.tsx
+++ src/components/layout/SemanticLayout.tsx
@@ -18,25 +18,19 @@ const SemanticLayout: React.FC<SemanticLayoutProps> = ({
   skipFooter = false
 }) => {
   return (
-    // Added this wrapper div with flex classes
     <div className="flex flex-col min-h-screen"> 
       {!skipNav && (
-        // Added z-50 to ensure header stays above content
         <header className="sticky top-0 z-50">
           <Navbar />
         </header>
       )}
       
-      {/* Added flex-grow to push footer down */}
       <main className={cn("main-content pt-[90px] lg:pt-[110px] flex-grow", mainClassName)}>
         {children}
       </main>
       
       {!skipFooter && (
-        // Added wrapper with mt-auto to push footer to bottom
-        <footer className="mt-auto">
-          <Footer />
-        </footer>
+        <Footer />
       )}
       
       {process.env.NODE_ENV === 'development' && (
