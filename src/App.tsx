aaf6dc772253d57e00cdcac79b7d230f13c1f49f
DataOps Group <GEOFF@dataopsgroup.com>
2025-06-04 11:34:28 -0500

    Fix multiple default exports in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,3 @@
-import { SpeedInsights } from '@vercel/speed-insights/next';
- 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <head>
-        <title>Next.js</title>
-      </head>
-      <body>
-        {children}
-        <SpeedInsights />
-      </body>
-    </html>
-  );
-}
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from '@/components/ui/sonner';
 import router from './routes';
