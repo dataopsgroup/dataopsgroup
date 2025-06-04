50223a096e0c1a34f2eb2742beb3b3819df56ac6
DataOps Group <GEOFF@dataopsgroup.com>
2025-06-04 11:20:35 -0500

    Remove all SSG-related code and configurations

--- vite.config.ts
+++ vite.config.ts
@@ -3,36 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
 import { componentTagger } from "lovable-tagger";
 
-// Define all routes for pre-rendering
-const routesToPrerender = [
-  '/',
-  '/about',
-  '/approach',
-  '/contact',
-  '/book',
-  '/services',
-  '/services/marketing-operations-revops',
-  '/services/analytics-bi',
-  '/services/dataops-implementation',
-  '/services/team-training',
-  '/insights',
-  '/assessment',
-  '/get-started',
-  '/thank-you',
-  '/contact/thank-you',
-  '/sitemap',
-  '/how-to-hire-a-hubspot-expert-in-2025',
-  '/pillar-content',
-  '/hubspot-assessment-results',
-  '/faqs',
-  '/faqs/hubspot-services',
-  '/faqs/hubspot-experts',
-  '/faqs/data-quality',
-  '/faqs/our-approach',
-  '/faqs/hubspot-modules',
-  '/404'
-];
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
